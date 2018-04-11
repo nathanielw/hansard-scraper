6f31d4830a63057b848c8977af80c77f7bd3bbca
Nathaniel Watson <nathaniel@nwatson.nz>
2018-04-11 21:07:22 +1200

    Remove hardcoded dates from search fetch URL

--- search-scraper.js
+++ search-scraper.js
@@ -4,7 +4,7 @@ const debug = require('debug')('hansard-scraper');
 
 const BASE_URL = 'https://www.parliament.nz';
 
-const START_PATH = '/en/ajax/hansardlisting/search/6227?Criteria.Timeframe=range&Criteria.DateFrom=2017-11-01&Criteria.DateTo=2018-03-18&Criteria.ViewAll=1&Criteria.Dt[0].Tick=false&Criteria.Dt[0].Value=Hansard - event&Criteria.Dt[1].Tick=true&Criteria.Dt[1].Value=Hansard - question&Criteria.Dt[2].Tick=false&Criteria.Dt[2].Value=Hansard - speech&Criteria.Dt[3].Tick=false&Criteria.Dt[3].Value=Hansard - vote&Criteria.ViewDetails=1';
+const START_PATH = '/en/ajax/hansardlisting/search/6227?Criteria.Timeframe=range&Criteria.ViewAll=1&Criteria.Dt[0].Tick=false&Criteria.Dt[0].Value=Hansard - event&Criteria.Dt[1].Tick=true&Criteria.Dt[1].Value=Hansard - question&Criteria.Dt[2].Tick=false&Criteria.Dt[2].Value=Hansard - speech&Criteria.Dt[3].Tick=false&Criteria.Dt[3].Value=Hansard - vote&Criteria.ViewDetails=1';
 
 const requestOptions = {
 	baseUrl: BASE_URL,
